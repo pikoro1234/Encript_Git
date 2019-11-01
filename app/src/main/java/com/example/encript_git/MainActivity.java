cc88a814fc9522c583a5b86f548907c2433b631a
pikoro1234 <donjuan.jfc@gmail.com>
2019-11-01 13:17:25 +0100

    metodos de encriptado y desencriptado de la clase RSA

--- app/src/main/java/com/example/encript_git/MainActivity.java
+++ app/src/main/java/com/example/encript_git/MainActivity.java
@@ -3,14 +3,69 @@ package com.example.encript_git;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //variables de bottones y textview
-    //private Button button;
+    private Button button;
+    private Button button_salida;
+    private Button button_create_xml;
+    private TextView text;
+    private TextView textEncoded;
+    private TextView textDecoded;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //asignado valor a elementos existentes en el DOM mediante el ID
+        this.text = findViewById(R.id.textEntrada);
+        this.textEncoded = findViewById(R.id.textSalida);
+        this.textDecoded = findViewById(R.id.textDesencriptado);
+        this.button = findViewById(R.id.btnEntrada);
+        this.button_salida = findViewById(R.id.btnSalida);
+        this.button_create_xml = findViewById(R.id.btnXml);
+    }
+
+    //metodo de encriptado
+    public void crypt(View view) {
+
+        try {
+            String password = this.text.getText().toString();
+            String encodedText;
+            RSA encodeRsa = new RSA();
+
+            encodeRsa.setContext(getBaseContext());
+            encodeRsa.genKeyPair(1024);
+            encodeRsa.saveToDiskPrivateKey("rsa.pri");
+            encodeRsa.saveToDiskPublicKey("rsa.pub");
+            encodedText = encodeRsa.Encrypt(password);
+            textEncoded.setText(encodedText);
+
+           /* writeToFile(createXML(password, encodedText, getApplicationContext()), getApplicationContext());
+            readFromFile(getApplicationContext());*/
+
+        }catch (Exception e) {
+            System.out.println("An error ocurred encrypting the password");
+        }
+    }
+
+    //metodo de desencriptado
+    public void descrypt(View view){
+        try{
+            String password_desct = this.textEncoded.getText().toString(); //this.text.getText().toString();
+            String decodedText;
+            RSA decodeRsa = new RSA();
+
+            decodeRsa.setContext(getBaseContext());
+            decodeRsa.openFromDiskPrivateKey("rsa.pri");
+            decodeRsa.openFromDiskPublicKey("rsa.pub");
+            decodedText = decodeRsa.Decrypt(password_desct);
+            textDecoded.setText(decodedText);
+        }catch (Exception e){
+            System.out.println("error al desencriptar");
+        }
     }
 
 
