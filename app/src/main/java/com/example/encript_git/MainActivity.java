303dbf6d5ac30772516665871b1e955452d7a383
pikoro1234 <donjuan.jfc@gmail.com>
2019-11-01 13:04:21 +0100

    creacion de clase RSA de encriptado y elementos del DOM

--- app/src/main/java/com/example/encript_git/MainActivity.java
+++ app/src/main/java/com/example/encript_git/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //variables de bottones y textview
+    //private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
